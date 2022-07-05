c761b7401e8c01c15a30099614aa8643a2053b39
cybernO0b <dm.lashkevich@gmail.com>
2022-07-06 03:25:12 +0500

    Добавлены итые по циклу картинки в карточки

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Фича написанная не собственноручно
-
 function loadData() {
     return new Promise((resolve, reject) => {
       setTimeout(resolve, 2000);
@@ -13,10 +11,22 @@ function loadData() {
       preloaderEl.classList.remove('visible');
     });
 
-
-
-
-
-
-
+const container = document.querySelector(".container");
+const main = document.querySelector("main")
+function viewCards(info){
+    for (let i = 0; i<info.length; i++){
+        const div = document.createElement("div");
+        
+        div.innerHTML = `
+    <div class="card__cat">
+    <div class="img__cat"><img src="imgcats/${i}.jpg" class="img__cat"></div>
+    
+        <div>${info[i].id}</div>
+        <div>${info[i].name}</div>
+    </div>`
+        main.append(div)
+    }
+    
+}
+viewCards(info)
 
